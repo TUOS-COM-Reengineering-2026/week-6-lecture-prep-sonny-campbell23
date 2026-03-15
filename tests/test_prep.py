ef381f76bcef05677567d00010c12337f0f0cb89
sonny-campbell23 <149419150+sonny-campbell23@users.noreply.github.com>
2026-03-15 18:59:26 +0000

    100% coverage

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -4,8 +4,38 @@ from prep import strange_function
 class MyTestCase(unittest.TestCase):
     def test_strange_function1(self):
         self.assertEqual(
+            first=strange_function(2, 2, 3, 4),
+            second='behaviour 1'
+        )
+    
+    def test_strange_function2(self):
+        self.assertEqual(
+            first=strange_function(1, 1, 4, 3),
+            second='behaviour 2'
+        )
+    
+    def test_strange_function3(self):
+        self.assertEqual(
             first=strange_function(1, 2, 3, 4),
             second='behaviour 3'
+        )
+    
+    def test_strange_function4(self):
+        self.assertEqual(
+            first=strange_function(2, 4, 1, 3),
+            second='behaviour 4'
+        )
+    
+    def test_strange_function5(self):
+        self.assertEqual(
+            first=strange_function(3, 1, 2, 4),
+            second='behaviour 5'
+        )
+
+    def test_strange_function6(self):
+        self.assertEqual(
+            first=strange_function(1, 2, 1, 3),
+            second='behaviour 6'
         )
 
     # TODO: Can you write more test cases below to increase the test coverage of `strange_function`?
